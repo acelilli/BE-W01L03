cfcf81714547190dbd59349b7c29cf33a7e1d61d
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-01-31 16:54:44 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/ContoCorrente.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ContoCorrente
+    {
+         // dati utente:
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        // dati conto:
+        private bool _contoPresente = false;
+        public bool ContoPresente
+        {
+            get { return _contoPresente; }
+            set { _contoPresente = value; }
+        }
+        public int Saldo { get; set; }
+        public int Id { get; set; }
+
+        // costruttore ContoCorrente:
+        /* public ContoCorrente(string nome, string cognome, bool contoPresente, int saldo, int id ) {
+            this.Nome = nome; 
+            this.Cognome = cognome;
+            this.ContoPresente = contoPresente;
+            this.Saldo = saldo;
+            this.Id = id;
+        } */
+        // METODI:
+        //1) Apertura conto -> CONDIZIONE: con importo iniziale 1000
+        public void ApriNuovoConto() // in questo caso è come se ApriNuovoConto faccia da costruttore
+        {
+            Console.WriteLine("Inserisci nome intestatario: \n");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci cognome intestatario: \n");
+            Cognome = Console.ReadLine();
+            ContoPresente = true;
+            Saldo = 1000;
+            Id = 1200024;
+            Console.WriteLine("\tConto corrente di: " + Nome + " " + Cognome + "\tSaldo attuale:" + Saldo + "\tId Conto:" + Id + "\t");
+            
+        }
+        //2) Versamento
+        public void Versamento()
+        {
+            Console.WriteLine("\nScegli importo da versare:");
+            Console.WriteLine("\na.50$ \nb.100$ \nc.200$ \nd.500$");
+            char add = Console.ReadKey().KeyChar; // leggo la key
+            int importo = 0; // istanza dell'importo prima dello switch
+                             //visto che ci sono +3 casi -> switch
+            switch (add)
+            { // a seconda della scelta do un valore a importo:
+                case 'a':
+                    importo = 50;
+                    break;
+                case 'b':
+                    importo = 100;
+                    break;
+                case 'c':
+                    importo = 200;
+                    break;
+                case 'd':
+                    importo = 500;
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
+            } Saldo += importo;
+            Console.WriteLine("\n Versamento di " + importo + " effettuato con successo.");
+            Console.WriteLine("\tConto corrente di: " + Nome + " " + Cognome + "\tSaldo attuale:" + Saldo + "\tId Conto:" + Id + "\t");
+        }
+        //3) Prelievo
+        public void Prelievo()
+        {
+            Console.WriteLine("\nScegli importo da prelevare:");
+            Console.WriteLine("\na.50$ \nb.100$ \nc.200$ \nd.500$");
+            char take = Console.ReadKey().KeyChar; // leggo la key
+            int importo = 0; // istanza dell'importo prima dello switch
+                             //visto che ci sono +3 casi -> switch
+            switch (take)
+            { // a seconda della scelta do un valore a importo:
+                case 'a':
+                    importo = 50;
+                    break;
+                case 'b':
+                    importo = 100;
+                    break;
+                case 'c':
+                    importo = 200;
+                    break;
+                case 'd':
+                    importo = 500;
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
+            }
+            Saldo -= importo;
+            Console.WriteLine("\n Prelievo di " + importo + " effettuato con successo.");
+            Console.WriteLine("\tConto corrente di: " + Nome + " " + Cognome + "\tSaldo attuale:" + Saldo + "\tId Conto:" + Id + "\t");
+        } 
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ESERCIZIO 1 -> CONTO CORRENTE
+            Console.WriteLine("Benvenuto in EpiBank \n scegli l'operazione da effettuare: \n");
+            // If else if else o switch in cui:
+            // 1 Aprire un conto 
+            // 2 Versare
+            // 3 Prelevare 
+            ContoCorrente mioConto = null; // dichiarazione istanza FUORI dal blocco condizionale o si blocca tutto
+            char scelta;
+
+            do
+            {
+                Console.WriteLine("\nPremi 1 per Aprire un conto \nPremi 2 per versare sul tuo conto \nPremi 3 per prelevare dal conto\n");
+                int choice = int.Parse(Console.ReadLine());
+                // APERTURA CONTO
+                if (choice == 1)
+                {
+                    Console.WriteLine("\nPer aprire il conto hai bisogno di un saldo iniziale di 1000$. \n Premi i per inserire:\n");
+                    char key = Console.ReadKey().KeyChar;
+                    if (key == 'i' || key == 'I')
+                    {
+                        Console.WriteLine("\nOperazione effettuata con successo.\n Apertura conto in corso...\n ");
+                        mioConto = new ContoCorrente();
+                        mioConto.ApriNuovoConto();
+                        Console.WriteLine("\nOperazione effettuata con successo!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nOperazione non effettuata. Riprovare?");
+                    }
+                }
+                // VERSAMENTO
+                else if (choice == 2)
+                {   // CONTROLLO: se il valore di mioConto non è più null E la proprietà ContoPresente è true
+                    if (mioConto != null && mioConto.ContoPresente == true)
+                    {   // allora lancio metodo versamento
+                        mioConto.Versamento();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nNon è possibile versare su un conto non esistente.");
+                    }
+                }
+                //PRELIEVO
+                else if (choice == 3)
+                {   // CONTROLLO: se il valore di mioConto non è più null E la proprietà ContoPresente è true
+                    if (mioConto != null && mioConto.ContoPresente == true)
+                    {   // allora lancio metodo prelievo
+                        mioConto.Prelievo();
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nNon è possibile prelevare da un conto non esistente.");
+                    }
+                }
+
+                Console.WriteLine("\nVuoi fare altro? (y / n)");
+                scelta = Console.ReadKey().KeyChar;
+
+            } while (scelta == 'y' || scelta == 'Y');
+
+            Console.ReadLine();
+        }
+    }
+}
